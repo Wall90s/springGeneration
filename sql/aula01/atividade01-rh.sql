e8f400fe74250ad8f040939a82b718b609a63dfb
Wallace <wallacesb@outlook.com.br>
2023-03-09 11:29:25 -0300

    Adicionando mais um atributo na Tabela de Colaboradores

--- sql/aula01/atividade01-rh.sql
+++ sql/aula01/atividade01-rh.sql
@@ -9,18 +9,19 @@ CREATE TABLE tb_colaboradores(
     idade INT(3),
     cargo VARCHAR(30),
     salario DECIMAL(10,2),
+    vt BOOLEAN,
     
     PRIMARY KEY(id)
 );
 
 SELECT * FROM tb_colaboradores;
 
-INSERT INTO tb_colaboradores(nome, idade, cargo, salario) 
-					  VALUES("Raquel Emilly da Rosa", 46, "Supervisora de Vendas", 10000),
-							("Vinicius Yago Noah Fogaça", 46, "Vendedor", 8000),
-                            ("Cauê Márcio Costa", 60, "Vendedor", 8000),
-                            ("Enzo Cauê Dias", 56, "Promotor de vendas", 9500),
-                            ("Esther Camila Vieira", 65, "Publicitária", 10000);
+INSERT INTO tb_colaboradores(nome, idade, cargo, salario, vt) 
+					  VALUES("Raquel Emilly da Rosa", 46, "Supervisora de Vendas", 10000, false),
+							("Vinicius Yago Noah Fogaça", 46, "Vendedor", 8000, true),
+                            ("Cauê Márcio Costa", 60, "Vendedor", 8000, true),
+                            ("Enzo Cauê Dias", 56, "Promotor de vendas", 9500, false),
+                            ("Esther Camila Vieira", 65, "Publicitária", 10000, true);
 
 SELECT nome FROM tb_colaboradores WHERE salario > 2000;
 
